41a3f8469593e96dcd1905bbe082420990993a51
Eduardo Orive <edorka@gmail.com>
2020-03-04 22:09:57 +0100

    fixed testing issues, a little late

--- src/DeviceMarker.test.js
+++ src/DeviceMarker.test.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { shallow } from 'enzyme';
+import ShallowRenderer from 'react-test-renderer/shallow'
 import DeviceMarker from "./DeviceMarker";
-import { Map } from "react-leaflet";
 
-it("renders learn react link", () => {
-  const data = {
-    Geometry: { Coordinates: [0, 1] }
-  };
-  const marker = shallow(<DeviceMarker {...data} />);
-  console.log(marker.toJSON());
-  expect(linkElement).toBeInTheDocument();
+describe('basic rendering', () => {
+
+    it("renders mark", () => {
+        const Geometry= { Coordinates: [0, 1] },
+          Temperature = 10,
+          Battery = 50,
+          Name = 'device_test',
+          data = {Geometry, Temperature, Battery, Name};
+        const component = enzyme.shallow(<DeviceMarker {...data} ></DeviceMarker>);
+        const marker = component.find('ForwardRef(Leaflet(Marker))')
+        expect(marker.props().position).toMatchObject(Geometry.Coordinates);
+        const info = marker.find('DeviceInfo')
+        expect(info.props()).toMatchObject({Name, Battery, Temperature});
+    });
+
 });
